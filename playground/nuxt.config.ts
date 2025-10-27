1eefeaaa8ed25003dfab6c7151753b3f77d37f0d
onmax <maximogarciamtnez@gmail.com>
2025-10-27 08:36:33 +0100

    Configure Nitro output for Vercel root

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,4 +2,7 @@ export default defineNuxtConfig({
   modules: ['../src/module', '@nuxtjs/tailwindcss'],
   devtools: { enabled: true },
   formisch: {},
+  nitro: {
+    output: { dir: '../.vercel/output' }
+  }
 })
